90cd59db57c0ccd2fdfc313af1473c416dd2dd02
10sr <8slashes+git@gmail.com>
2012-11-30 03:38:06 +0900

    add fd.sh, which manipulate fd by exec command

---
+++ fd.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# open input fd
+if test -n "$1"
+then
+    exec 3<$1
+else
+    exec 3<&0
+fi
+
+while read -r line 0<&3
+do
+    echo line:$line
+done
+
+exec 3<&-
+
+# open output fd
+if test -n "$2"
+then 
+    exec 4>$2
+else
+    exec 4>&1
+fi
+
+echo aaa 1>&4
+
+exec 4>&-
