2c8deb15969d497530abfc954d07593023654b01
10sr <8slashes+git@gmail.com>
2012-12-03 19:51:35 +0900

    add py_completer

---
+++ py_completer/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+try:
+    import readline
+except ImportError:
+    print("Module readline not available.")
+    readline = None
+else:
+    if "libedit" in readline.__doc__ :
+        readline.parse_and_bind("bind ^I rl_complete")
+    else :
+        readline.parse_and_bind("tab: complete")
+
+def compl1(text, state) :
+    if state == 0 :
+        return "text "
+    else :
+        return None
+
+def main() :
+    readline.set_completer(compl1)
+    print(readline.get_completer_delims())
+    while True :
+        s = input("input str: ")
+        print(s)
+        if s == "" :
+            break
+
+main()
