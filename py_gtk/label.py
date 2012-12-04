5a022a8a321ced47cc513e835a42abbb853dff24
10sr <8slashes+git@gmail.com>
2012-12-04 20:38:18 +0900

    add label.py, which test label and button press event

---
+++ py_gtk/label.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+
+# example helloworld.py
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+gdk = gtk.gdk
+
+class HelloWorld:
+
+    # This is a callback function. The data arguments are ignored
+    # in this example. More on callbacks below.
+    def hello(self, widget, event, data=None):
+        print("Hello World")
+        if event.type == gdk.BUTTON_PRESS :
+            print("button pressed.")
+            if event.button == 1 :
+                print("left button prssed.")
+            elif event.button == 3 :
+                print("right button pressed.")
+        return
+
+    def delete_event(self, widget, event, data=None):
+        # If you return FALSE in the "delete_event" signal handler,
+        # GTK will emit the "destroy" signal. Returning TRUE means
+        # you don't want the window to be destroyed.
+        # This is useful for popping up 'are you sure you want to quit?'
+        # type dialogs.
+        print("delete event occurred")
+
+        # Change FALSE to TRUE and the main window will not be destroyed
+        # with a "delete_event".
+        return False
+
+    def destroy(self, widget, data=None):
+        print "destroy signal occurred"
+        gtk.main_quit()
+
+    def __init__(self):
+        # create a new window
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+
+        # When the window is given the "delete_event" signal (this is given
+        # by the window manager, usually by the "close" option, or on the
+        # titlebar), we ask it to call the delete_event () function
+        # as defined above. The data passed to the callback
+        # function is NULL and is ignored in the callback function.
+        self.window.connect("delete_event", self.delete_event)
+
+        # Here we connect the "destroy" event to a signal handler.  
+        # This event occurs when we call gtk_widget_destroy() on the window,
+        # or if we return FALSE in the "delete_event" callback.
+        self.window.connect("destroy", self.destroy)
+
+        self.window.set_border_width(10)
+        self.label = gtk.Label("alabel")
+        self.label.show()
+
+        self.evbox = gtk.EventBox()
+        self.evbox.add(self.label)
+        self.evbox.connect("button_press_event", self.hello, None)
+        self.evbox.show()
+
+        self.window.add(self.evbox)
+        self.window.show()
+
+    def main(self):
+        # All PyGTK applications must have a gtk.main(). Control ends here
+        # and waits for an event to occur (like a key press or mouse event).
+        gtk.main()
+
+# If the program is run directly or passed as an argument to the python
+# interpreter then create a HelloWorld instance and show it
+if __name__ == "__main__":
+    hello = HelloWorld()
+    hello.main()
