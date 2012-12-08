8fd7416b71b536d5c1dbac18524dd8b9cd618ed3
10sr <8slashes+git@gmail.com>
2012-12-08 22:03:55 +0900

    add ordereddict.py

---
+++ ordereddict.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2
+
+from collections import OrderedDict
+
+od = OrderedDict()
+
+od["a"] = 1
+od["b"] = 2
+od["c"] = 4
+od["d"] = 5
+del od["a"]
+od["a"] = 3
+
+for k, v in od.iteritems() :
+    print("{},{}".format(k,v))
+print("")
+
+# print :
+# b,2
+# c,4
+# d,5
+# a,3
+# OrderedDict is FIFO
+
+for k in reversed(od) :         # reversed is not destructive
+    print("{},{}".format(k,od[k]))
+print("")
+
+# a,3
+# d,5
+# c,4
+# b,2
+
+for k, v in od.iteritems() :
+    print("{},{}".format(k,v))
+print("")
+
+# b,2
+# c,4
+# d,5
+# a,3
