df2c42eaee1dc1369a7b71aa0e3e7502e2e289a8
10sr <8slashes+git@gmail.com>
2013-03-06 14:41:46 +0900

    write page

--- html_load_local_sqlite/load.js
+++ html_load_local_sqlite/load.js
@@ -1,16 +1,15 @@
-$.get("test.txt", null, function(data, textStatus){
-    console.log(data);
-}, "text");
+// $.get("test.txt", null, function(data, textStatus){
+//     console.log(data);
+// }, "text");
 $(function(){
     $("#fileInfo").click(function(){
-        console.log($("#myFile"));
         $("#myFile").each(function(){
-            var fileData = this.files[0]; // how to get file object?
+            var fileData = this.files[0];
             var txt = "name: "+fileData.name+"<br />";
             txt += "size: "+fileData.size+"<br />";
             txt += "type: "+fileData.type+"<br />";
             console.log(txt);
-            // $("#result").innerHTML = txt;
+            $("#result").html(txt);
         });
     });
 });
