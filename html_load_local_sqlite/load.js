d6f05b7e0c98fc619ba0fb5c5b52df5b535bc687
10sr <8slashes+git@gmail.com>
2013-03-06 11:48:16 +0900

    fileapi test

--- html_load_local_sqlite/load.js
+++ html_load_local_sqlite/load.js
@@ -1,6 +1,19 @@
 $.get("test.txt", null, function(data, textStatus){
-    alert(data);
+    console.log(data);
 }, "text");
+$(function(){
+    $("#fileInfo").click(function(){
+        console.log($("#myFile"));
+        $("#myFile").each(function(){
+            var fileData = this.files[0]; // how to get file object?
+            var txt = "name: "+fileData.name+"<br />";
+            txt += "size: "+fileData.size+"<br />";
+            txt += "type: "+fileData.type+"<br />";
+            console.log(txt);
+            // $("#result").innerHTML = txt;
+        });
+    });
+});
 // $(function () {
 //     $("#data").load("test.txt");
 // }
