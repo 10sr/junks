821d2dae60b8290aedf4219e1fcb02338f48be47
10sr <8slashes+git@gmail.com>
2013-03-07 21:32:32 +0900

    small fix

--- html_load_local_sqlite/load.js
+++ html_load_local_sqlite/load.js
@@ -1,13 +1,19 @@
 // $.get("test.txt", null, function(data, textStatus){
 //     console.log(data);
 // }, "text");
+
 $(function(){
+    // initialize field
+    function initField(){
+        ;
+    };
+
     $("#fileInfo").click(function(){
-        console.log($("#myFile").get(0).files[0].name);
+        // http://thinkit.co.jp/story/2013/02/06/3953
         var fileData = $("#myFile").get(0).files[0];
-        var txt = "name: "+fileData.name+"<br />";
-        txt += "size: "+fileData.size+"<br />";
-        txt += "type: "+fileData.type+"<br />";
+        var txt = "name: " + fileData.name + "<br />";
+        txt += "size: " + fileData.size + "<br />";
+        txt += "type: " + fileData.type + "<br />";
         console.log(txt);
         $("#result").html(txt);
     });
