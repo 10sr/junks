774d8eb9890b8c7d6211a23d7bf7f4413cfb14b7
10sr <8slashes+git@gmail.com>
2013-05-01 16:00:52 +0900

    add sizeof

---
+++ ctest/sizeof.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#define P(type) int s_##type = sizeof(type);  \
+  printf(#type ": %d\n", s_##type)
+
+int main(void){
+  /* int s_pointer = sizeof(void*); */
+  /* int s_int = sizeof(int); */
+  /* int s_double = sizeof(double); */
+
+  /* printf("int: %d, pointer: %d\n", s_int, s_pointer); */
+  P(int);
+  P(float);
+  P(double);
+  return 0;
+}
