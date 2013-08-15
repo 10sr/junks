96dbfd31da7988d9242b1a566be369d1c5c80b22
10sr <8slashes+git@gmail.com>
2013-08-15 23:51:28 +0900

    update hashify.me

--- hashify.me/README.md
+++ hashify.me/README.md
@@ -1,14 +1,25 @@
 hashify.py
 ==========
 
-Python client of <hashify.me> service.
+Python client for [hashify.me](http://hashify.me) service.
+
+Developed by [10sr](https://github.com/10sr), at
+<https://github.com/10sr/junks/tree/master/hashify.me> .
 
 
-bitly.com
----------
 
-I got :
+Usage
+=====
 
-    b'{ "data": [ ], "status_code": 500, "status_txt": "MISSING_ARG_ACCESS_TOKEN" }'
+    $ hashify [<filename>]
 
-????????????? MISSING????????????
+If a filename is given, read contents from that file. Otherwise, contents is
+read from stdin. Bitly-shortened URL will be printed out.
+
+
+bit.ly
+======
+
+By default, my (10sr) account is used for shorten URLs.
+You can use your own access token if you want. Your access token is available
+at <https://bitly.com/a/oauth_apps> .
--- hashify.me/hashify.py
+++ hashify.me/hashify.py
@@ -12,21 +12,31 @@ def bitly(url):
     assert bitly_id
     assert bitly_key
 
-    import http.client, urllib.parse
-    Import json
+    # import http.client
+    import urllib.parse, urllib.request
+    import json
 
-    params = urllib.parse.urlencode({"access_token" : bitly_key,
-                                     "longUrl" : url,
-                                     "domain" : "j.mp"})
+    params = urllib.parse.urlencode({
+        "longUrl" : url,
+        "domain" : "j.mp",
+        "access_token" : bitly_key
+    })
     # headers = {"Content-type": "application/x-www-form-urlencoded",
     #            "Accept": "text/plain"}
-    headers = {"Accept": "text/plain"}
 
-    conn = http.client.HTTPSConnection(BITLY_API_HOST)
-    conn.request("GET", "/v3/shorten", params, headers)
-    res = conn.getresponse()
-    res = res.read()
-    return res
+    # i wont use HTTPConnection any more
+    # conn = http.client.HTTPSConnection(BITLY_API_HOST)
+    # print(params)
+    # conn.request("GET", "/v3/shorten", "access_token=aa", headers)
+
+    url = "https://{host}{path}?{params}".format(host=BITLY_API_HOST,
+                                                path="/v3/shorten",
+                                                params=params)
+    res = urllib.request.urlopen(url)
+    assert res.status == 200
+    resbody = json.loads(res.read().decode("utf-8"))
+    assert resbody["status_code"] == 200
+    return resbody["data"]["url"]
 
 def hashify(infile=sys.stdin):
     s = infile.read()
@@ -34,11 +44,15 @@ def hashify(infile=sys.stdin):
 
     from base64 import b64encode
     result_url = HASHIFY_URL + "/" + b64encode(b).decode("utf-8")
-    print(result_url)
     return result_url
 
 def main(argv):
-    result = hashify(sys.stdin)
+    if len(argv) > 1:
+        with open(argv[1]) as f:
+            result = hashify(f)
+    else:
+        result = hashify(sys.stdin)
+
     if bitly_id and bitly_key:
         result = bitly(result)
     print(result)
