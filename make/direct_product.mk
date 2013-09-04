873d8cb399d78ba208341ed7939a1ef4adde067f
10sr <8slashes+git@gmail.com>
2013-09-04 21:10:34 +0900

    update make test

--- make/direct_product.mk
+++ make/direct_product.mk
@@ -4,7 +4,8 @@ result := abc def
 
 # what i want: result == abc-aaa abc-bbb def-aaa def-bbb
 
-result := $(foreach elem,$(a),$(result:%=%-$(elem)))
+result_a := $(foreach elem,$(a),$(result:%=%-$(elem)))
+result += $(result_a)
 
 default:
 	 echo $(result)
