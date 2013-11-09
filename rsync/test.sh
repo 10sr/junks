9278557a8c8ceaee9f15bbe9f105e3760e05f437
10sr <8slashes+git@gmail.com>
2013-11-10 00:46:56 +0900

    add test2

--- rsync/test.sh
+++ rsync/test.sh
@@ -3,7 +3,8 @@ set -e
 
 # test.sh --- Test the usage of rsync
 
-basedir="$PWD/rsynctest_base"
+# basedir="$PWD/rsynctest_base"
+basedir="./rsynctest_base"
 
 show_dirtree(){
     # http://www.math.kobe-u.ac.jp/~kodama/tips-dirtree.html
@@ -13,6 +14,11 @@ show_dirtree(){
         find . | sort | \
         sed -ne '1b;s/[^\/]*\//+--/g;s/+--+/|  +/g;s/+--+/|  +/g;s/+--|/|  |/g;p'
     )
+}
+
+rsync(){
+    echo :: rsync "$@"
+    command rsync "$@"
 }
 
 initialize(){
@@ -35,42 +41,65 @@ initialize(){
 }
 
 isfile(){
-    printf "Checking file $1..."
+    # printf "Checking file $1..."
     if test -f "$1"
     then
-        echo "passed."
+        true
+        # echo "passed."
     else
-        echo "failed."
+        echo "Checking file $1 failed."
         return 1
     fi
 }
 
 isdir(){
-    printf "Checking dir $1..."
+    # printf "Checking dir $1..."
     if test -d "$1"
     then
-        echo "passed."
+        true
+        # echo "passed."
     else
-        echo "failed."
+        echo "Checking dir $1 failed."
         return 1
     fi
 }
 
 test1(){
-    # rsync -av dir1 dir2 where dir2 does not exists
-    rsync -av "$basedir/src" "$basedir/dst1"
+    echo "-----test1-------------------"
+    # rsync -a dir1 dir2 where dir2 does not exists
+    rsync -a "$basedir/src" "$basedir/dst1"
 }
 
 test1_check(){
+    echo "-----test1 check-------------"
     isfile "$basedir/dst1/src/a.txt"
     isfile "$basedir/dst1/src/b.txt"
     isdir "$basedir/dst1/src/d"
     isfile "$basedir/dst1/src/d/b.txt"
+    show_dirtree "$basedir/dst1"
+    echo
+}
+
+test2(){
+    echo "-----test2-------------------"
+    # rsync -a dir1/ dir2 where dir2 does not exists
+    rsync -a "$basedir/src/" "$basedir/dst2"
+}
+
+test2_check(){
+    echo "-----test2 check-------------"
+    isfile "$basedir/dst2/a.txt"
+    isfile "$basedir/dst2/b.txt"
+    isdir "$basedir/dst2/d"
+    isfile "$basedir/dst2/d/b.txt"
+    show_dirtree "$basedir/dst2"
+    echo
 }
 
 main(){
     initialize
     test1 && test1_check
+    test2 && test2_check
 }
 
 main
