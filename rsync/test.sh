42839c5f887e3e4189ebd723332096667bee9507
10sr <8slashes+git@gmail.com>
2013-11-24 13:19:35 +0900

    rsync: fix test.sh

--- rsync/test.sh
+++ rsync/test.sh
@@ -70,77 +70,74 @@ isdir(){
 }
 
 test1(){
-    echo "-----test1-------------------"
     # rsync -a dir1 dir2 where dir2 does not exists
     debug rsync -a "$basedir/src" "$basedir/dst1"
 }
 
 test1_check(){
-    echo "-----test1 check-------------"
     isfile "$basedir/dst1/src/a.txt"
     isfile "$basedir/dst1/src/b.txt"
     isdir "$basedir/dst1/src/d"
     isfile "$basedir/dst1/src/d/b.txt"
     show_dirtree "$basedir/dst1"
-    echo
 }
 
 test2(){
-    echo "-----test2-------------------"
     # rsync -a dir1/ dir2 where dir2 does not exists
     debug rsync -a "$basedir/src/" "$basedir/dst2"
 }
 
 test2_check(){
-    echo "-----test2 check-------------"
     isfile "$basedir/dst2/a.txt"
     isfile "$basedir/dst2/b.txt"
     isdir "$basedir/dst2/d"
     isfile "$basedir/dst2/d/b.txt"
     show_dirtree "$basedir/dst2"
-    echo
 }
 
 test3(){
-    echo "-----test3-------------------"
     # rsync -a dir1 dir3 where dir3 exists
     debug mkdir -p "$basedir/dst3"
     debug rsync -a "$basedir/src" "$basedir/dst3"
 }
 
 test3_check(){
-    echo "-----test3 check-------------"
     isfile "$basedir/dst3/src/a.txt"
     isfile "$basedir/dst3/src/b.txt"
     isdir "$basedir/dst3/src/d"
     isfile "$basedir/dst3/src/d/b.txt"
     show_dirtree "$basedir/dst3"
-    echo
 }
 
 test4(){
-    echo "-----test4-------------------"
     # rsync -a dir1/ dir4 where dir4 exists
     debug mkdir -p "$basedir/dst4"
     debug rsync -a "$basedir/src/" "$basedir/dst4"
 }
 
 test4_check(){
-    echo "-----test3 check-------------"
     isfile "$basedir/dst4/a.txt"
     isfile "$basedir/dst4/b.txt"
     isdir "$basedir/dst4/d"
     isfile "$basedir/dst4/d/b.txt"
     show_dirtree "$basedir/dst4"
-    echo
+}
+
+test_check(){
+    # test_check <num>
+    echo "-----test$1-------------------" && \
+        test${1} && \
+        echo "-----test$1 check-------------" && \
+        test${1}_check && \
+        echo
 }
 
 main(){
     initialize
-    test1 && test1_check
-    test2 && test2_check
-    test3 && test3_check
-    test4 && test4_check
+    test_check 1
+    test_check 2
+    test_check 3
+    test_check 4
 }
 
 main
