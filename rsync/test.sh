d9abc5d6ea9f309b433a9a43ad694bb676ca3b25
10sr <8slashes+git@gmail.com>
2013-11-24 13:46:30 +0900

    rsync: add 3 tests

--- rsync/test.sh
+++ rsync/test.sh
@@ -6,13 +6,16 @@ set -e
 # basedir="$PWD/rsynctest_base"
 basedir="./rsynctest_base"
 
+####################################
+# utils
+
 show_dirtree(){
     # http://www.math.kobe-u.ac.jp/~kodama/tips-dirtree.html
     if test -f "$1"
     then
-        echo ":: $1 is a file."
+        echo ":: show_dirtree: $1 is a file."
     else
-        echo ":: Dir tree of $1"
+        echo ":: show_dirtree: Dir tree of $1"
         (
             cd "$1" && \
                 find . | sort | \
@@ -24,25 +27,6 @@ show_dirtree(){
 debug(){
     echo :: "$@"
     "$@"
-}
-
-initialize(){
-    # this create the dir tree like:
-    # +--src
-    # |  +--a.txt
-    # |  +--b.txt
-    # |  +--d
-    # |  |  +--b.txt
-
-    test -d "$basedir" && rm -r "$basedir"
-    mkdir -p "$basedir"
-    mkdir "$basedir/src"
-    echo a >"$basedir/src/a.txt"
-    echo b >"$basedir/src/b.txt"
-    mkdir "$basedir/src/d"
-    echo c >"$basedir/src/d/b.txt"
-    show_dirtree "$basedir/src"
-    echo
 }
 
 isfile(){
@@ -68,6 +52,31 @@ isdir(){
         return 1
     fi
 }
+
+#####################################
+# initialization
+
+initialize(){
+    # this create the dir tree like:
+    # +--src
+    # |  +--a.txt
+    # |  +--b.txt
+    # |  +--d
+    # |  |  +--b.txt
+
+    test -d "$basedir" && rm -r "$basedir"
+    mkdir -p "$basedir"
+    mkdir "$basedir/src"
+    echo a >"$basedir/src/a.txt"
+    echo b >"$basedir/src/b.txt"
+    mkdir "$basedir/src/d"
+    echo c >"$basedir/src/d/b.txt"
+    show_dirtree "$basedir/src"
+    echo
+}
+
+######################################
+# tests
 
 test1(){
     # rsync -a dir1 dir2 where dir2 does not exists
@@ -123,6 +132,38 @@ test4_check(){
     show_dirtree "$basedir/dst4"
 }
 
+test5(){
+    # rsync -a file path where path not exists
+    debug rsync -a "$basedir/src/a.txt" "$basedir/dst5"
+}
+
+test5_check(){
+    isfile "$basedir/dst5"
+    show_dirtree "$basedir/dst5"
+}
+
+test6(){
+    # rsync -a file path/ where path not exists
+    debug rsync -a "$basedir/src/a.txt" "$basedir/dst6/"
+}
+
+test6_check(){
+    isdir "$basedir/dst6"
+    isfile "$basedir/dst6/a.txt"
+    show_dirtree "$basedir/dst6"
+}
+
+test7(){
+    # rsync -a file path/subdir/ where path not exists
+    debug rsync -a "$basedir/src/a.txt" "$basedir/dst7/subdir/"
+}
+
+test7_check(){
+    isdir "$basedir/dst7"
+    isfile "$basedir/dst7/subdir/a.txt"
+    show_dirtree "$basedir/dst7"
+}
+
 test_check(){
     # test_check <num>
     echo "-----test$1-------------------" && \
@@ -138,6 +179,9 @@ main(){
     test_check 2
     test_check 3
     test_check 4
+    test_check 5
+    test_check 6
+    test_check 7
 }
 
 main
