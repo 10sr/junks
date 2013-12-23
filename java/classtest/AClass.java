12aa8c4a3f59ecd25e098a93864cac77c8012930
10sr <8slashes+git@gmail.com>
2013-12-23 20:45:14 +0900

    add some static codes

--- java/classtest/AClass.java
+++ java/classtest/AClass.java
@@ -1,26 +1,37 @@
 public class AClass{
     int a;
+    // static var is shared among instances
+    static int s;
+
+    static{
+        AClass.s = 1;
+    }
+
+    public static void sets(int b)
+    {
+        AClass.s = b;
+    }
 
     // met of same name as class name is constructor
     public AClass(int b)
     {
-        a = b;
+        this.a = b;
     }
 
     public void seta(int b)
     {
-        a = b;
+        this.a = b;
         return;
     }
 
     public int geta()
     {
-        return a;
+        return this.a;
     }
 
     // many classes have equals method
     public boolean equals(AClass c)
     {
-        return a == c.a;
+        return this.a == c.a;
     }
 }
--- java/classtest/Test.java
+++ java/classtest/Test.java
@@ -4,12 +4,22 @@ class Test{
     public static void main(String arg[])
     {
         AClass c = new AClass(3);
-        // c.met1(2);
         int a = c.a;
         System.out.println("hello world");
         System.out.println(a);
 
+        c.seta(2);
+        System.out.println(c.a);
+
         AClass c2 = new AClass(4);
         System.out.println(c.equals(c2));
+
+        System.out.println(c.s);
+        c.sets(5);
+        System.out.println(c2.s);
+
+        // BClass d = new BClass();
+        // d.seta(0);
+        // System.out.println(d.a);
     }
 }
