48d4502046c5deb7439ee4352ac1a82abade415e
10sr <8slashes+git@gmail.com>
2013-12-23 20:06:40 +0900

    add classtest of java

---
+++ java/classtest/AClass.java
@@ -0,0 +1,13 @@
+public class AClass{
+    int a;
+    public void met1(int b)
+    {
+        a = b;
+        return;
+    }
+
+    public int met2()
+    {
+        return a;
+    }
+}
---
+++ java/classtest/Test.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Test{
+    public static void main(String arg[])
+    {
+        AClass c = new AClass();
+        c.met1(2);
+        int a = c.a;
+        System.out.println("hello world");
+        System.out.println(a);
+    }
+}
