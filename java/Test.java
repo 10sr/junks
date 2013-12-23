9d75d5fbe654cdbec658a1273f374102f2c8974d
10sr <8slashes+git@gmail.com>
2013-12-24 01:11:30 +0900

    add String tests

--- java/Test.java
+++ java/Test.java
@@ -4,5 +4,9 @@ class Test{
     public static void main(String arg[])
     {
         System.out.println("hello world");
+
+        String str = "abcde";
+        char str2 = str.charAt(2);
+        System.out.println(str2);
     }
 }
