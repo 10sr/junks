7ad31a506b51437ea788073bf4a3d3730d61415a
10sr <8slashes+git@gmail.com>
2014-01-12 00:18:54 +0900

    add pthread_key test

---
+++ c/pthread_key.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<pthread.h>
+
+#define THREAD_NUM 3
+
+pthread_key_t g_key;
+
+void _func_called_from_th(void)
+{
+    intptr_t i;
+    i = (intptr_t) pthread_getspecific(g_key);
+    printf("i am %ld.\n", (intptr_t) i);
+    return;
+}
+
+void *_thread_func(void *_arg)
+{
+    pthread_setspecific(g_key, _arg);
+    _func_called_from_th();
+    return NULL;
+}
+
+int main(int argc, char **argv)
+{
+    pthread_t th[THREAD_NUM];
+    intptr_t i;
+
+    pthread_key_create(&g_key, NULL);
+
+    for (i = 0; i < THREAD_NUM; i++) {
+        pthread_create(&(th[i]), NULL, _thread_func, (void *)i);
+    }
+
+    for (i = 0; i < THREAD_NUM; i++) {
+        pthread_join(th[i], NULL);
+    }
+
+    return 0;
+}
