b6b948d360ba193a40695aee7b852a7f1da2f45a
10sr <8slashes+git@gmail.com>
2014-01-15 12:40:27 +0900

    add MultiInitClass

---
+++ java/classtest/MultiInitClass.java
@@ -0,0 +1,26 @@
+public class MultiInitClass{
+    private int a = 0;
+    private double b = 0.0;
+
+    public MultiInitClass()
+    {
+        return;
+    }
+
+    public MultiInitClass(int a)
+    {
+        this.a = a;
+    }
+
+    public MultiInitClass(double b)
+    {
+        this.b = b;
+    }
+
+    public void print()
+    {
+        System.out.println("a: " + this.a);
+        System.out.println("b: " + this.b);
+        return;
+    }
+}
