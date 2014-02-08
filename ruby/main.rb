4ee73dcea72bb8ecc32e384e65a16ea7c014a180
10sr <8slashes+git@gmail.com>
2014-02-08 18:42:31 +0900

    ruby: add some test code

--- ruby/main.rb
+++ ruby/main.rb
@@ -4,8 +4,8 @@
 p "abc"
 p 1
 p [1, 3, 4]
-p({"a" => 1, "b" => 2})
 p :abc
+p({:a => 1, :b => 2})
 
 # toplevel method definition
 def met(a, b)
@@ -28,3 +28,52 @@ rescue RuntimeError, SyntaxError => evar
 end
 
 met2
+
+class AClass
+  def initialize(a1, a2)
+    @m1, @m2 = a1, a2
+  end
+
+  def met1()
+    p "met1 of AClass"
+    p "m1: " + @m1
+  end
+end
+
+o1 = AClass.new("abc", 2)
+o1.met1()
+
+# inherit
+class BClass < AClass
+  def met2()
+    p "calling met1 two times"
+    # call met1 of AClass two times
+    met1
+    self.met1
+    p "done"
+  end
+end
+
+o2 = BClass.new("def", 3)
+o2.met2
+
+# try to call nonexisting method
+begin
+  o2.met3("ghi")
+rescue NoMethodError => evar
+  p evar
+  p "nonexisting method called."
+end
+
+# add method to existing class
+class BClass
+  def met3(a)
+    @m1 = a
+    p "set m1 and calling met1"
+    met1
+    p "done"
+  end
+end
+
+o2.met3("ghi")
+o2.met1
