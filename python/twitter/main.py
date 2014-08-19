3c8fbc4956acb41a24394e6a46950ae33726d6fd
10sr <8slashes+git@gmail.com>
2014-08-19 14:18:24 +0900

    twitter: update

--- python/twitter/main.py
+++ python/twitter/main.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, os
+
+import twitter as tw
 
 def main(argv):
+    TOKEN_FILE = os.path.expanduser("~/.jtwitter.token")
+    CONSUMER_KEY = "QnuQ9aJRkWsZywky93QF4Dr4a"
+    CONSUMER_SECRET = "Ri960YKmMGGM1ga667lVUL4aELPLOcVd0ORvxUVXQkyVyqaeWL"
+
+    if not os.path.exists(TOKEN_FILE):
+        tw.oauth_dance("j/python/twitter", CONSUMER_KEY, CONSUMER_SECRET,
+                       TOKEN_FILE)
+
+    oauth_token, oauth_secret = tw.read_token_files(TOKEN_FILE)
+
+    tw.statuses.update("@tos j/p/t test")
     return 0
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     sys.exit(main(sys.argv))
