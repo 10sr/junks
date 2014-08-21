059a14f0c617c90349150654974b2fefc63faf46
10sr <8slashes+git@gmail.com>
2014-08-21 16:45:13 +0900

    python/twitter: accept args for status to update

--- python/twitter/main.py
+++ python/twitter/main.py
@@ -1,23 +1,44 @@
 #!/usr/bin/env python3
 
+TOKEN_FILE = "~/.jtwitter.token"
+CONSUMER_KEY = "QnuQ9aJRkWsZywky93QF4Dr4a"
+CONSUMER_SECRET = "Ri960YKmMGGM1ga667lVUL4aELPLOcVd0ORvxUVXQkyVyqaeWL"
+
 import sys, os
 
 import twitter as tw
 
-def main(argv):
-    TOKEN_FILE = os.path.expanduser("~/.jtwitter.token")
-    CONSUMER_KEY = "QnuQ9aJRkWsZywky93QF4Dr4a"
-    CONSUMER_SECRET = "Ri960YKmMGGM1ga667lVUL4aELPLOcVd0ORvxUVXQkyVyqaeWL"
+TOKEN_FILE = os.path.expanduser(TOKEN_FILE)
 
+def ensure_auth():
     if not os.path.exists(TOKEN_FILE):
         tw.oauth_dance("j/python/twitter", CONSUMER_KEY, CONSUMER_SECRET,
                        TOKEN_FILE)
+    return
+
+def update_status(text):
+    text = text.strip()
+    if not text:
+        raise RuntimeError("No text to update")
 
     oauth_token, oauth_secret = tw.read_token_file(TOKEN_FILE)
 
     t = tw.Twitter(auth=tw.OAuth(oauth_token, oauth_secret,
                                  CONSUMER_KEY, CONSUMER_SECRET))
-    t.statuses.update(status="@tos j/p/t test")
+
+    t.statuses.update(status=text)
+
+    return
+
+def main(argv):
+
+    ensure_auth()
+
+    if sys.stdin.isatty():
+        update_status(input("Status: "))
+    else:
+        update_status(sys.stdin.read())
+
     return 0
 
 if __name__ == "__main__":
