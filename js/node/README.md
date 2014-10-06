f8695285eb0e6aa9e1997929c965a75f36ef5128
10sr <8slashes+git@gmail.com>
2014-10-06 22:00:46 +0900

    js/node: Update myhttp.js and update document about require

--- js/node/README.md
+++ js/node/README.md
@@ -10,3 +10,16 @@ npm
 By default, `npm install` installs npm modules "locally": and do not touch
 global environments.
 The Directory is `node_modules`.
+
+
+require
+-------
+
+Type `global.module.paths` in interactive node prompt to print module search
+paths.
+
+
+Refs
+-----
+* [Nodeビギナーズブック » Node.jsチュートリアル » Node.js 教程](http://www.nodebeginner.org/index-jp.html)
+* [npm と Node.js 上で require を使ったモジュール読み込みの仕組みについてメモ | TM Life](http://tmlife.net/programming/javascript/npm-node-js-require-module-memo.html)
--- js/node/myhttp.js
+++ js/node/myhttp.js
@@ -1,6 +1,27 @@
+#!/usr/bin/env node
+// myhttp.js
+// http://www.nodebeginner.org/index-jp.html
+
 var http = require('http');
-http.createServer(function (req, res) {
+var url = require('url');
+
+function onReqWOListener(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    console.log("Request for " + pathname + " Recieved!");
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hell, World\n');
-}).listen(1337, '192.168.56.101');
+}
+
+// not work with GET?
+function onReqWithListener(req, res) {
+    req.addListener("end", function(){
+        var pathname = url.parse(req.url).pathname;
+        console.log("Request for " + pathname + " Recieved!");
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hell, World\n');
+    });
+}
+
+http.createServer(onReqWOListener).listen(1337, '192.168.56.101');
+
 console.log('Server running at http://192.168.56.101:1337');
