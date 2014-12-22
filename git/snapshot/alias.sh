2aa16f1a45c4ea1fd5cfa16f78c95772a1f51d61
10sr <8slashes+git@gmail.com>
2014-12-22 17:50:55 +0900

    Add comment about git stash snapshot

--- git/snapshot/alias.sh
+++ git/snapshot/alias.sh
@@ -9,5 +9,6 @@
 git config --local alias.snapshot '! gitdir="`git rev-parse --git-dir`" && export GIT_INDEX_FILE="$gitdir"/snapshot.index && cp "$gitdir"/index "$GIT_INDEX_FILE" && git add -A && : >>"$gitdir"/logs/refs/snapshot && git update-ref refs/snapshot $(git commit-tree $(git write-tree) -m Snapshot -p HEAD)'
 
 # snapshot which utilizes git stash create
+# Problem: git stash create do not index untracked files
 
 git config --local alias.stashsnap '! gitdir="`git rev-parse --git-dir`" && : >>"$gitdir"/logs/refs/snapshot && git update-ref refs/snapshot $(git stash create)'
