0752e4d73d8519d27443cb36d881080fe61ca25c
10sr <8slashes+git@gmail.com>
2015-01-02 03:36:43 +0900

    Add test script

---
+++ ansible/test/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+pwd="`dirname "$0"`"
+echo $pwd
+
+cd "$pwd/.."
+
+git submodule update --init
+
+# Fix for arch linux where python is symlink to python3
+find ansible -type f -name '*.py' \
+     -exec sed -ie 's|#!/usr/bin/env python|#!/usr/bin/env python2|' {} +
+sed -ie 's/python setup.py egg_info/python2 setup.py egg_info/' \
+    ansible/hacking/env-setup
+
+
+
+# Start test
+. ansible/hacking/env-setup
+
+set -x
+for f in library/*
+do
+    python2 ansible/hacking/test-module -m "$f"
+done
