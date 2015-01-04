92046918133076a02a21cd1268497ecab8f6b974
10sr <8slashes+git@gmail.com>
2015-01-04 15:19:45 +0900

    Update comment

--- ansible/library/boilerplate.py
+++ ansible/library/boilerplate.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# ansible module boilerplate example
 # http://docs.ansible.com/developing_modules.html#common-module-boilerplate
 
 
@@ -26,8 +27,14 @@ def main():
     return 0
 
 
-# *Must* use astarisk, or fail with
+# *Must* use astarisk, or fail when test-module with
 # "this module requires key=value arguments (['<<INCLUDE_ANSIBLE_MODULE_ARGS>>'])"
+
 # http://www.backlog.jp/blog/2013/12/ansible-module-2.html
+# Python import does not actually occur, instead ansible runtime replaces this
+# line with contents in ansible.module_utils.basic.py .
+# Replacing code appears in this file:
+# https://github.com/ansible/ansible/blob/devel/lib/ansible/module_common.py
 from ansible.module_utils.basic import *
+# https://github.com/ansible/ansible/blob/devel/lib/ansible/module_utils/basic.py
 main()
