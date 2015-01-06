a4b31df67c02adce52c3e183a26d5c66c6129b4a
10sr <8slashes+git@gmail.com>
2015-01-06 19:23:53 +0900

    Add comment about async

--- js/node/async/index.js
+++ js/node/async/index.js
@@ -4,6 +4,11 @@ var async = require('async');
 
 var tasks = [];
 
+// I have a question: I know that I cannot know which function finish first
+// (how order done() is called), but how about the *calls* of functions?
+// In this case the `console.log()`s are assured to be called in the order
+// they appear???
+
 tasks.push(function(done){
   console.log('First tasks called');
   done(new Error('error of first task'), 0);
