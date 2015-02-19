4434cd13d78dbdcbbc9afe56dd5d44821e5bb8db
10sr <8slashes+git@gmail.com>
2015-02-19 17:35:32 +0900

    Add uglify example

--- js/node/gulp-try/README.md
+++ js/node/gulp-try/README.md
@@ -3,3 +3,9 @@ j/js/node/gulp-try
 
 Try gulp.js features.
 
+
+
+References
+-----------
+
+* http://qiita.com/shuhei/items/4aaa550d1ad30d2fc3b5
--- js/node/gulp-try/gulpfile.js
+++ js/node/gulp-try/gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function(){
+gulp.task('uglify', function(){
+  gulp.src('./src/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dst'));
+});
+
+gulp.task('consolelog', function(){
   console.log('Task named default ran');
 });
+
+gulp.task('default', ['consolelog', 'uglify']);
