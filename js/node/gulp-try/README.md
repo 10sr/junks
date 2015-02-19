58c8143bb41380e53c15c8bfd11ea7f73054f189
10sr <8slashes+git@gmail.com>
2015-02-19 18:01:32 +0900

    Add task dependency example

--- js/node/gulp-try/README.md
+++ js/node/gulp-try/README.md
@@ -9,3 +9,4 @@ References
 -----------
 
 * http://qiita.com/shuhei/items/4aaa550d1ad30d2fc3b5
+* https://github.com/gulpjs/gulp/tree/master/docs/recipes
--- js/node/gulp-try/gulpfile.js
+++ js/node/gulp-try/gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
-gulp.task('uglify', function(){
+gulp.task('consolelog', function(){
+  console.log('Task named default ran');
+});
+
+
+// task dependencies
+// Run order: msguglify -> uglify
+
+gulp.task('uglify', ['msguglify'], function(){
   gulp.src('./src/**/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('dst'));
 });
 
-gulp.task('consolelog', function(){
-  console.log('Task named default ran');
+gulp.task('msguglify', function(done){
+  console.log('Running uglify');
+  done();
 });
 
-gulp.task('default', ['consolelog', 'uglify']);
+gulp.task('default', ['uglify']);
