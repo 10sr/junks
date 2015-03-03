1d8bd42246395aaa2516d5438539a06c4854b5ae
10sr <8slashes+git@gmail.com>
2015-03-03 14:55:55 +0900

    Add some sample

--- php/preg_replace.php
+++ php/preg_replace.php
@@ -10,4 +10,18 @@ $r2 = preg_replace($rx, "", $s2);
 var_dump($r1);
 var_dump($r2);
 
+
+#########################
+
+# multiple lines
+$s3 = "abcdef
+ghi jil
+mmmmmm";
+
+$rx3 = "/.*ghi.*/";
+
+$r3 = preg_replace($rx3, "", $s3);
+
+var_dump($r3);
+
 ?>
