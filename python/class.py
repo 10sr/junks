f8be79ea1fe1458aa5fd70ebf7824a100c7cea44
10sr <8slashes+git@gmail.com>
2015-05-25 10:41:34 +0900

    Add one study

--- python/class.py
+++ python/class.py
@@ -7,9 +7,29 @@ class MyClass:
         print(s)
 
     def met(self):
-        print("bcd")
-        self.func("aaab")
+        print("MyClass.met")
+        self.func("Call MyClass.func from MyClass.met")
 
 print("bc")
 c = MyClass()
 c.met()
+
+
+# Add method lately
+
+class MyClass2:
+    def met1(self):
+        print("MyClass2.meth1")
+
+
+def met_outside(self, s):
+    print("met_outside")
+    print(s)
+    print(self)
+
+
+c2 = MyClass2()
+
+MyClass2.met_outside = met_outside
+
+c2.met_outside("ab")
