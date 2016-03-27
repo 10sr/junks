be40610558c881988205cc2ccc6ef573e2dce9d0
10sr <8slashes+git@gmail.com>
2016-03-28 01:13:03 +0900

    Update groovy script

--- groovy/README.md
+++ groovy/README.md
@@ -4,3 +4,9 @@ j/groovy
 Run simple script
 
     groovy main.groovy
+
+References
+---------
+
+* http://kakutani.com/trans/ociweb/jnbFeb2004.html
+* http://qiita.com/opengl-8080/items/a0bb31fb20cb6505188b
--- groovy/main.groovy
+++ groovy/main.groovy
@@ -1,1 +1,14 @@
 println "this is groovy script!"
+println("this also prints a string")
+
+
+// list
+aList = ["elem1", "elem2"]
+
+println aList
+// puts [elem1, elem2]
+
+aList << "elem3"
+
+println aList
+// puts [elem1, elem2, elem3]
