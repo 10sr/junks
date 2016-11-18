5bf5093ab7cb3d1853c13b3f98d68be9fb1326b5
10sr <8slashes+git@gmail.com>
2016-11-18 15:16:48 +0900

    Update hello.go

--- golang/hello.go
+++ golang/hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+)
 
 func main(){
 	fmt.Printf("hello, world\n")
+
+	p, e := filepath.Abs("")
+
+	fmt.Printf("%v\n", p)
+	fmt.Printf("%v\n", e)
 }
