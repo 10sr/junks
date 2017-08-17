65bedb101553ffb1424847aa72d12233999877f4
10sr <8slashes+git@gmail.com>
2017-08-17 13:23:14 +0900

    Add foldLeft example

--- scala/AProject/src/main/scala/a.scala
+++ scala/AProject/src/main/scala/a.scala
@@ -5,5 +5,20 @@ object AProject {
     println("abc")
     val a = new B("DDD")
     a.show
+
+    val m = Map("hoge" -> "fuga", "hoe" -> "fue")
+    val s = Seq("mohe", "fuge")
+
+    s.zipWithIndex.map {
+      case (e, i) => println(s"e: ${e}, i: ${i}")
+    }
+
+    val m1 = s.zipWithIndex.foldLeft(m) {
+      case (m, (e, i)) => {
+        println(s"m: ${m}, e: ${e}, i: ${i}")
+        m.updated(e, i.toString)
+      }
+    }
+    println(m1)
   }
 }
