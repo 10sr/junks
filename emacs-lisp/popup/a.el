c2380f83db562a090fe5a0453e06d91d59f0d95e
10sr <8.slashes@gmail.com>
2018-08-29 15:50:47 +0900

    Update popup

--- emacs-lisp/popup/a.el
+++ emacs-lisp/popup/a.el
@@ -1,8 +1,12 @@
 (require 'popup)
 
+;; http://stackoverflow.com/questions/13242165/ddg#13242340
+(setq popup-use-optimized-column-computation nil)
+
 (popup-menu* '(1 2 3) :prompt "hoe: " :point (point-min))
 
-(popup-cascade-menu '(("Top1" "Sub1" "Sub2") "Top2") :point (point-min))
+(message "%s"
+         (popup-cascade-menu '(("Top1" "Sub1" "Sub2") "Top2") :point (point-min)))
 
 ;; Local Variables:
 ;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
