d4eb683dd5d13fcc38ab0f354cc00f33d86db8bf
10sr <8.slashes@gmail.com>
2018-09-18 16:21:33 +0900

    Fix

--- awk/until_this_line.sh
+++ awk/until_this_line.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # https://stackoverflow.com/questions/23934486/is-a-start-end-range-expression-ever-useful-in-awk
+# https://stackoverflow.com/questions/17908555/printing-with-sed-or-awk-a-line-following-a-matching-pattern#17914105
 
 echo "abc
 def
