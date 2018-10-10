4b7346e96b602de0855fd1f6358c3537f5ca80c7
10sr <8.slashes@gmail.com>
2018-10-10 13:22:01 +0900

    Update arg.py

--- python/arg.py
+++ python/arg.py
@@ -6,9 +6,15 @@ import sys
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Try argparse functionality')
+
     parser.add_argument('--args',
                         nargs='+',
                         help='multiple args test')
+
+    parser.add_argument("--choices",
+                        choices=["a", "b", "c"],
+                        help="Arg with choice test")
+
     args = parser.parse_args(argv[1:])
     print(args)
     return
