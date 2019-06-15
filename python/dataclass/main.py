2b19535313278069fc1228ca7056f496b3f5e3ad
10sr <8.slashes@gmail.com>
2019-06-16 03:22:10 +0900

    Add type check

--- python/dataclass/main.py
+++ python/dataclass/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-# from __future__ import annotations
+from __future__ import annotations
 
+from typing import Optional
 import dataclasses
 from pprint import pprint
 import typing
@@ -12,6 +13,7 @@ class AClass:
     aattr: str
     battr: int = 2
     list_attr: list = dataclasses.field(default_factory=list)
+    #o_p_t: Optional[int] = None
 
     normal_attr = "hoehoe"
 
@@ -21,14 +23,21 @@ class AClass:
         pprint(dataclasses.fields(self))
         pprint(dataclasses.asdict(self))
         pprint(dataclasses.astuple(self))
+
+        # We need explicit type check
+        # When from __future__ import annotations, field.type is a str
+        types = typing.get_type_hints(self)
+        for field in dataclasses.fields(self):
+            assert isinstance(getattr(self, field.name), types[field.name]), f"Type check fail: {field.name}"
         return
 
     @classmethod
     def from_dict(cls, dict_):
-        # We need explicit type check
-        for field in dataclasses.fields(cls):
-            if field.name in dict_:
-                assert isinstance(dict_[field.name], field.type)
+        # types = typing.get_type_hints(cls)
+        # pprint(types)
+        # for field in dataclasses.fields(cls):
+        #     if field.name in dict_:
+        #         assert isinstance(dict_[field.name], types[field.name])
         return cls(**dict_)
 
 
@@ -37,7 +46,8 @@ class AClass:
 a = AClass("aaa")
 b = AClass.from_dict({
     "aattr": "momomo",
-    # "battr": "mememe"  # This need explicit type check to fail
+    "battr": "mememe",  # This need explicit type check to fail
+    #"o_p_t": "m",
     # "a": True  # This raises error
 })
 #print(a)
