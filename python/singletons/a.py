1bde83494d4dde090f6d23b47dbc5b650f1e42e7
10sr <8.slashes@gmail.com>
2019-06-19 23:56:54 +0900

    Add singletons

---
+++ python/singletons/a.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+from typing import Optional
+
+# https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_singleton.htm
+class SingletonCheckInInit:
+    __singleton_instance: Optinal[SingletonCheckInInit] = None
+
+    @classmethod
+    def get_instance(cls) -> SingletonCheckInInit:
+        if cls.__singleton_instance is None:
+            cls.__singleton_instance = cls()
+        return cls.__singleton_instance
+
+    def __init__(self):
+        if self.__singleton_instance is not None:
+            raise RuntimeError("Singleton!")
+        return
+
+print(SingletonCheckInInit.get_instance())
+print(SingletonCheckInInit.get_instance())
+
+try:
+    SingletonCheckInInit()
+except Exception as e:
+    print(e)
+
+
+# https://blanktar.jp/blog/2016/07/python-singleton.html
+class SingletonHackNew:
+    __singleton_instance = None
+
+    def __init__(self):
+        print('init')
+        return
+
+    def __new__(cls):
+        if cls.__singleton_instance is None:
+            cls.__singleton_instance = super(SingletonHackNew, cls).__new__(cls)
+
+        return cls.__singleton_instance
+
+print(SingletonHackNew())
+print(SingletonHackNew())
+
+
+# http://www.denzow.me/entry/2018/01/28/171416
+class SingletonAnotherNew:
+    __singleton_instance = None
+
+    def __new__(cls):
+        raise RuntimeError('Cannot initialize via Constructor')
+
+    @classmethod
+    def __internal_new__(cls):
+        return super(SingletonAnotherNew, cls).__new__(cls)
+
+    @classmethod
+    def get_instance(cls):
+        if not cls.__singleton_instance:
+            cls.__singleton_instance = cls.__internal_new__()  # 変更
+
+        return cls.__singleton_instance
+
+print(SingletonAnotherNew.get_instance())
+print(SingletonAnotherNew.get_instance())
+
+try:
+    SingletonAnotherNew()
+except Exception as e:
+    print(e)
