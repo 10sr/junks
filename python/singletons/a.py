26df9f044d28b3df9b3c6d9f71f754f9aeaa76a0
10sr <8.slashes@gmail.com>
2019-06-20 14:08:34 +0900

    Add document

--- python/singletons/a.py
+++ python/singletons/a.py
@@ -6,6 +6,13 @@ from typing import Optional
 
 # https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_singleton.htm
 class SingletonCheckInInit:
+    """
+    Raise error in __init__ when already instanciated.
+
+    __new__ をハックしない。
+    はじめの一度は普通のインスタンス化が成功する。
+    ２度め以降は get_instance() を使用する必要がある。
+    """
     __singleton_instance: Optinal[SingletonCheckInInit] = None
 
     @classmethod
@@ -30,6 +37,13 @@ except Exception as e:
 
 # https://blanktar.jp/blog/2016/07/python-singleton.html
 class SingletonHackNew:
+    """
+    Do not create new instance in __new__ method.
+
+    通常のインスタンス化で必ず同一のオブジェクトを返す。
+    get_instance のようなメソッドを使用しない。
+    __init__ はオブジェクト取得時に毎回呼ばれる。
+    """
     __singleton_instance = None
 
     def __init__(self):
@@ -48,6 +62,12 @@ print(SingletonHackNew())
 
 # http://www.denzow.me/entry/2018/01/28/171416
 class SingletonAnotherNew:
+    """
+    Raise error in __new__ method and define another new method.
+
+    __internal_new__ メソッドを別に作って get_instance からはそちらを呼ぶ。
+    別のメソッドを作らないで get_instance から直接 super 呼べばいい気がするので無駄な気がする。
+    """
     __singleton_instance = None
 
     def __new__(cls):
@@ -78,6 +98,13 @@ except Exception as e:
 
 
 class SingletonDisallowInit:
+    """
+    Raise error in __init__ method.
+
+    get_instance からは super.__new__ を呼ぶ。
+    別に super でなく単に cls.__new__ を呼べばいいのでは？
+    それをしたのが SingletonDisallowInit2 。
+    """
     __singleton_instance = None
 
     def __init__(self):
@@ -101,6 +128,11 @@ except Exception as e:
 
 
 class SingletonDisallowInit2:
+    """
+    Raise error in __init__ and only use __new__ method.
+
+    get_instance は単に __init__ を迂回して __new__ だけを呼ぶ。
+    """
     __singleton_instance = None
 
     def __init__(self):
@@ -124,6 +156,12 @@ except Exception as e:
 
 
 class SingletonDisallowNew:
+    """Raise error in __new__ method.
+
+    SingletonAnotherNew の改変版。
+    the() からは super.__new__ を呼ぶ。
+    ついでに __init__ も呼ばれなくなってるが直感に反する気がする。
+    """
     __singleton_instance = None
 
     def __new__(self):
