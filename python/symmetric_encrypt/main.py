fdf56e7fb58da3b38276100176b9f80c0ca34d8d
10sr <8.slashes@gmail.com>
2019-09-24 16:11:43 +0900

    Update symnetric_encrypt

--- python/symmetric_encrypt/main.py
+++ python/symmetric_encrypt/main.py
@@ -1,30 +1,43 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-# Not work?
-# http://bityard.blogspot.jp/2010/10/symmetric-encryption-with-pycrypto-part.html
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-import binascii
-from Crypto.Cipher import AES
+def gen_key(password: bytes, salt: bytes) -> bytes:
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
+    return key
 
+print(gen_key(b"password", b"salt"))
+print(gen_key(b"password", b"salt_"))
 
-def enc(string, key):
-    aes = AES.new(key, AES.MODE_ECB)
-    return aes.encrypt(string.decode("utf-8")).encode("hex")
+msg = b"target_message"
 
+from cryptography.fernet import Fernet
 
-def dec(string, key):
-    aes = AES.new(key, AES.MODE_ECB)
-    return aes.decrypt(binascii.unhexlify(string))
+def e(key: bytes, msg: bytes) -> bytes:
+    f = Fernet(key)
+    return f.encrypt(msg)
 
+def d(key: bytes, msg: bytes) -> bytes:
+    f = Fernet(key)
+    return f.decrypt(msg)
 
-if __name__ == "__main__":
-    text = "hogehoge"
-    key = "hoehoe"
+encrypted = e(gen_key(b"password", b"salt"), msg)
+decrypted = d(gen_key(b"password", b"salt"), encrypted)
 
-    print(text)
+print(encrypted)
+print(decrypted)
 
-    encrypted = enc(text, key)
-    print(encrypted)
-
-    decrypted = dec(encrypted, key)
-    print(decrypted)
+# Fail!
+decrypted_2 = d(gen_key(b"password", b"salt_"), encrypted)
+print(decrypted_2)
