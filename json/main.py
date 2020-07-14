0bf703432085a7cef615e99858b674d22aaf749d
10sr <8.slashes@gmail.com>
2020-07-14 13:27:46 +0900

    Update json

--- json/main.py
+++ json/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import json
+import sys
 from pprint import pprint
 
-with open("a.json") as f:
+with open(sys.argv[1]) as f:
     o = json.load(f)
     pprint(o)
-    print(o["hoehoe"])
+    # print(o["hoehoe"])
