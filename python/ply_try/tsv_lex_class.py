d9365d6b3cdb8eba3f124ee8fff6cddf933dd2b2
10sr <8.slashes@gmail.com>
2021-02-26 15:04:19 +0900

    Add tsv_lex_class

---
+++ python/ply_try/tsv_lex_class.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# https://ply.readthedocs.io/en/latest/ply.html#lex-example
+from ply import lex
+
+class TsvParser(object):
+    _lexer = None
+
+    tokens = (
+        "FIELD",
+        "TAB",
+        "NEWLINE",
+    )
+
+    t_FIELD = r"[a-zA-Z0-9]+"
+    t_TAB = "[\t]"
+    t_NEWLINE = "[\n]"
+
+    # Error handling rule
+    def t_error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+        return
+
+    # Build the lexer
+    def build(self, **kwargs):
+        self._lexer = lex.lex(module=self, **kwargs)
+        return
+
+    def __iter__(self):
+        return iter(self._lexer)
+
+    def input(self, data):
+        self._lexer.input(data)
+        return self._lexer
+
+
+tsv_parser = TsvParser()
+tsv_parser.build()
+print(list(tsv_parser.input("aaaa\t100\tmomomo\n")))
