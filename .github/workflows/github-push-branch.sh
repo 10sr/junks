0ec48147cb63bb46441fa869edb2a2c8445e2ea8
10sr <8.slashes@gmail.com>
2021-08-16 16:36:29 +0900

    Fix script

--- .github/workflows/github-push-branch.sh
+++ .github/workflows/github-push-branch.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 git submodule deinit -f ansible/ansible_src || true
-git rm -f ansible/ansible_src
+git rm -f ansible/ansible_src || true
 rm -rf .git/modules/ansible/ansible
 
 date >index.html
