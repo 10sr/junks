2825f6e2936d7d18cdfcbe3b326e8ffb1afaaa7f
10sr <8.slashes@gmail.com>
2021-09-06 18:28:11 +0900

    Add click_try

---
+++ python/click_try/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import click
+
+
+@click.command()
+@click.pass_context
+@click.argument("n")
+@click.option("-p", help="oPtion")
+def cmd1(ctx, n, p):
+    print(n)
+    print(p)
+    print(ctx.obj)
+    return
+
+@click.group()
+@click.pass_context
+@click.argument("m")
+@click.option("-o", help="Option")
+def cli(ctx, m, o):
+    print(m)
+    print(o)
+    ctx.ensure_object(dict)
+    ctx.obj["m"] = m
+    ctx.obj["o"] = o
+    return
+
+cli.add_command(cmd1)
+
+if __name__ == "__main__":
+    # cli(["--help"])
+    cli(["-o", "fue", "hoe", "cmd1", "abc"])
