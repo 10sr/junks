d8f9647e824796e26bdd50b712df2362dedf4d52
10sr <8.slashes@gmail.com>
2021-09-23 09:32:40 +0900

    Update

--- python/click_try/main.py
+++ python/click_try/main.py
@@ -1,11 +1,40 @@
 #!/usr/bin/env python3
 
+import functools
+
 import click
 
+def f1(f):
+    print(f"{f.__doc__=}")
 
+    @functools.wraps(f)
+    def wrapper(*args, **kargs):
+        print(f"{args=}")
+        print(f"{kargs=}")
+        print(f"{f.__doc__=}")
+        # print(f"{f.__code__=}")
+        print(f"{dir(f)=}")
+        # print(f"{dir(args[0])=}")
+        res = f(*args, **kargs)
+        return res
+    # print(f"{f.__dict__=}")
+    # print(f"{wrapper.__dict__=}")
+    print(f"{f.params=}")
+    print(f"{dir(f.params[0])=}")
+    print(f"{f.params[0].help=}")
+    f.params[0].help = "HOEHOEHOE"
+    print(f"{f.params[0].help=}")
+    attrs_f = set(dir(f))
+    attrs_wrapper = set(dir(wrapper))
+    for attr in (attrs_f - attrs_wrapper):
+        setattr(wrapper, attr, (getattr(f, attr)))
+    return wrapper
+
+
+@f1
 @click.command()
 @click.pass_context
-@click.argument("n")
+@click.argument("n", help="n help")
 @click.option("-p", help="oPtion")
 def cmd1(ctx, n, p):
     click.echo(n)
@@ -15,7 +44,7 @@ def cmd1(ctx, n, p):
 
 @click.group()
 @click.pass_context
-@click.argument("m")
+@click.argument("m", help="m help")
 @click.option("-o", help="Option")
 def cli(ctx, m, o):
     click.echo(m)
