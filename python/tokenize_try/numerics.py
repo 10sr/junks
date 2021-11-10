d91b8c47bd7947a4d6705d74189835b33ed1d63b
10sr <8.slashes@gmail.com>
2021-11-11 08:17:21 +0900

    Add

---
+++ python/tokenize_try/numerics.py
@@ -0,0 +1,10 @@
+# https://www.python.org/dev/peps/pep-0515/
+print(123_456)
+print(10_000_000.0)
+print(0xCAFE_F00D)
+print(0b_0011_1111_0100_1110)
+
+print(10_000_000.000_000_1)
+print(10_000_000.000_000_1e3)
+
+# print(0x10_000_000.000_000_1)
