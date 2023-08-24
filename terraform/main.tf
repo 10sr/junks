eaa7eb13b007cb7685a71e3765cb5b6748115eb7
10sr <8.slashes@gmail.com>
2023-08-24 13:11:09 +0900

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,20 @@ variable "s1" {
   default = ["hoe", "fue", 1]
 }
 
+locals {
+  m1 = {
+    "k1" = {},
+    "k2" = {
+      p1 = 1
+    },
+  }
+  m1_keys = toset(keys(local.m1))
+}
+
 output "s1" {
   value = var.s1
+}
+
+output "m1" {
+  value = local.m1_keys
 }
